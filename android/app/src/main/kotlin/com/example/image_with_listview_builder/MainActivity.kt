89abdac86929917ced5fd2d05d12610c618ecef9
alhasann351 <alhasann352@gmail.com>
2024-02-11 17:38:57 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/image_with_listview_builder/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.image_with_listview_builder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: const Color(0xFF59B4C3),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+  
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        title: const Text('Image With ListView Builder'),
+      ),
+      body: const Text('Hello Flutter Developer'),
+    );
+  }
+}
